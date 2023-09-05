8214ca9522fbe07a6f5b35bb08645b1bea29a616
Marcelo de Azevedo <marcelobazevedo@gmail.com>
2023-09-05 13:37:26 -0300

    PROJETO TERMINADO

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -3,25 +3,44 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Clicker extends Component
 {
+    use WithPagination;
 
-    public $name;
-    public $email;
-    public $password;
+    #[Rule('required|min:2|max:50')]
+    public $name = '';
+
+    #[Rule('required|email|unique:users')]
+    public $email = '';
+
+    #[Rule('required|min:2')]
+    public $password = '';
+
     public function createNewUser()
     {
+        ;
+        $validated = $this->validate();
         User::create([
-            'name'=>$this->name,
-            'email'=> $this->email,
-            'password'=>$this->password
+//            'name'=>$this->name,
+//            'email'=> $this->email,
+//            'password'=>$this->password
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
         ]);
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'Usuario criado com sucesso!');
     }
+
     public function render()
     {
-        $users = User::all();
+        $users = User::paginate(2);
         return view('livewire.clicker', [
             'users' => $users
         ]);
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,21 +1,16 @@
-<!doctype html>
-<html class="h-full bg-white">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
-<body class="h-full">
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        @if(session('success'))
+            <span class="px-3 py-3 bg-green-600 text-white rounded">{{session('success')}}</span>
+        @endif
         <form wire:submit="createNewUser" class="space-y-6">
             <div>
                 <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nome</label>
                 <div class="mt-2">
                     <input wire:model="name" type="text"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                @error('name')
-                    <span class="text-danger-500 text-xs">{{$message}}</span>
+                    @error('name')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
                     @enderror
                 </div>
             </div>
@@ -24,7 +19,11 @@
                 <div class="mt-2">
                     <input wire:model="email" type="email"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
                 </div>
+                @error('email')
+                <span class="text-red-500 text-xs">{{$message}}</span>
+                @enderror
             </div>
 
             <div>
@@ -34,23 +33,25 @@
                 <div class="mt-2">
                     <input wire:model="password" type="password"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    @error('password')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
+                    @enderror
                 </div>
             </div>
 
             <div>
-                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Cadastrar
                 </button>
             </div>
         </form>
     </div>
-    <br >
-    <hr />
+    <br>
+    <hr/>
     @foreach($users as $user)
         <h3>{{$user->name}}</h3>
     @endforeach
+    {{$users->links('vendor.livewire.test')}}
 </div>
-
-</body>
-</html>
 
